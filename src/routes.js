b33031c47d5f05605c7c98d7b4fee08242474743
CARLOS PAQUE NUÑEZ <cpaquen@caixabanktech.com>
2023-10-19 19:23:04 +0200

    service and routes cleanup

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,11 @@
 import React, { lazy, Suspense } from 'react';
 import { Routes as Routing, Route } from 'react-router-dom';
-import LandingPage from './pages/LandingPage';
 import Loading from './components/Loading/Loading';
 import MavContextProvider from './context/MavContext';
 import Header from './components/Header/Header';
 
 // Lazy load pages and components
-// const AboutPage = lazy(() => import('./pages/AboutPage'));
-// const PromptPage = lazy(() => import('./pages/PromptPage'));
 const UploadDocument = lazy(() => import('./pages/UploadDocument'));
-// const ResultsPage = lazy(() => import('./pages/ResultsPage'));
 const FeaturesPage = lazy(() => import('./pages/FeaturesPage'));
 
 function Routes() {
@@ -17,25 +13,6 @@ function Routes() {
     <MavContextProvider>
       <Header />
       <Routing>
-        <Route exact path="/landing" element={<LandingPage />} />
-        {/* <Route
-          exact
-          path="/about"
-          element={
-            <Suspense fallback={<Loading />}>
-              <AboutPage />
-            </Suspense>
-          }
-        />
-        <Route
-          exact
-          path="/prompts"
-          element={
-            <Suspense fallback={<Loading />}>
-              <PromptPage />
-            </Suspense>
-          }
-        /> */}
         <Route
           exact
           path="/"
@@ -45,15 +22,6 @@ function Routes() {
             </Suspense>
           }
         />
-        {/* <Route
-          exact
-          path="/results"
-          element={
-            <Suspense fallback={<Loading />}>
-              <ResultsPage />
-            </Suspense>
-          }
-        /> */}
         <Route
           exact
           path="/features"
--- src/services/request-service.js
+++ src/services/request-service.js
@@ -1,94 +1,11 @@
 import axios from 'axios';
-import MockAdapter from 'axios-mock-adapter';
-import epicsAI from '../mocks/epicsAI.json';
-import diagram from '../assets/diagram.png';
+// import epicsAI from '../mocks/epicsAI.json';
 
 const RequestService = {
-  get: (url, body) => {
-    const mock = new MockAdapter(axios, { delayResponse: 2000 });
-
-    mock.onGet(url).reply(200, {
-      message: 'Request successful',
-      data: body,
-    });
-    return axios
-      .get(url, body)
-      .then((response) => {
-        return response;
-      })
-      .catch((error) => {
-        return error;
-      });
-  },
-  getEpics: (url) => {
-    const mock = new MockAdapter(axios, { delayResponse: 2000 });
-
-    mock.onGet(url).reply(200, epicsAI);
-
-    return axios
-      .get(url)
-      .then((response) => {
-        return response;
-      })
-      .catch((error) => {
-        return error;
-      });
-  },
-  post: (url, body) => {
-    const mock = new MockAdapter(axios, { delayResponse: 2000 });
-
-    mock.onPost(url).reply(200, {
-      message: 'Request successful',
-      data: body,
-    });
-
-    return axios
-      .post(url, body)
-      .then((response) => {
-        return response;
-      })
-      .catch((error) => {
-        return error;
-      });
-  },
-  postMermaid: (url, body) => {
-    const mock = new MockAdapter(axios, { delayResponse: 2000 });
-
-    mock.onPost(url).reply(200, {
-      message: 'Request successful',
-      data: diagram,
-    });
-
-    return axios
-      .post(url, body)
-      .then((response) => {
-        return response;
-      })
-      .catch((error) => {
-        return error;
-      });
-  },
-  put: (url, body) => {
-    const mock = new MockAdapter(axios, { delayResponse: 2000 });
-
-    mock.onPut(url).reply(200, {
-      message: 'Request successful',
-      data: body,
-    });
-
-    return axios
-      .put(url, body)
-      .then((response) => {
-        return response;
-      })
-      .catch((error) => {
-        return error;
-      });
-  },
   postDocument: (url, body, type) => {
-    const mock = new MockAdapter(axios, { delayResponse: 5000 });
-
-    mock.onPost(url).reply(200, epicsAI);
+    // Uncomment these if you want a mocked response!
+    // const mock = new MockAdapter(axios, { delayResponse: 5000 });
+    // mock.onPost(url).reply(200, epicsAI);
 
     const getConfig = () => {
       const configArr = {
